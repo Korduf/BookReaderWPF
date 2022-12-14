dc8a4f00d9ca9048b903d4977c97420eb0e06457
Jomblish <jomblish@mail.ru>
2022-12-14 17:01:34 +0300

    v0.4

--- INIManager.cs
+++ INIManager.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 using System.Text;
 
 namespace DeepReadApp
 {
+    [SuppressMessage("ReSharper", "IdentifierTypo")]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class INIManager
     {
         [DllImport("kernel32.dll", EntryPoint = "GetPrivateProfileString")]
@@ -11,31 +13,24 @@ namespace DeepReadApp
         [DllImport("kernel32.dll", EntryPoint = "WritePrivateProfileString")]
         private static extern int WritePrivateString(string section, string key, string str, string path);
         
-        private string path = null;
+        private readonly string _path;
         private const int Size = 1024;
 
-        public string Path
-        {
-            get { return path; }
-            set { path = value; }
-        }
-        
-        
         public INIManager(string outerpath)
         {
-            path = outerpath;
+            _path = outerpath;
         }
 
         public string GetPrivateString(string oSection, string oKey)
         {
-            StringBuilder buffer = new StringBuilder(Size);
-            GetPrivateString(oSection, oKey, null, buffer, Size, path);
+            var buffer = new StringBuilder(Size);
+            GetPrivateString(oSection, oKey, null, buffer, Size, _path);
             return buffer.ToString();
         }
 
         public void WritePrivateString(string oSection, string oKey, string oValue)
         {
-            WritePrivateString(oSection, oKey, oValue, path);
+            WritePrivateString(oSection, oKey, oValue, _path);
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,44 +1,31 @@
 using System;
-using System.Net.Mime;
 using System.Windows;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Windows.Markup;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
-using System.Configuration;
-using System.Windows.Annotations;
-using System.Windows.Controls.Primitives;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 
 namespace DeepReadApp
 {
+    [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
     public partial class MainWindow
     {
         private INIManager _iniManager = new INIManager(@"./ApplicationSettings.ini");
         private List<string> _pathBookLibrary = new List<string>();
         private List<double> _listMarkZoom = new List<double>();
         private List<int> _listMarkPage = new List<int>();
-        private TextBox _textBoxDelMark = new TextBox();
         private WrapPanel _wrapPanel = new WrapPanel();
         private StackPanel _panel = new StackPanel();
-        private Button _buttonDelMark = new Button();
-        private Window _windowDelMark = new Window();
         private Window _windowLibrary = new Window();
-        private bool IsOpen { get; set; } = false;
+        private bool IsOpen { get; set; }
         private string Extension { get; set; }
         private string FilePath { get; set; }
         private string FileName { get; set; }
@@ -52,11 +39,10 @@ namespace DeepReadApp
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
             LoadBookLibrary();
-            RemoveMarkWindowInitialize();
             LibraryWindowInitialize();
         }
 
-        public void ExitApp(object sender, EventArgs e)
+        private void ExitApp(object sender, EventArgs e)
         {
             SavePositionInBook_INI();
             SaveMark();
@@ -66,13 +52,13 @@ namespace DeepReadApp
 
         private void LoadFileDialog(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == false) return;
             SavePositionInBook_INI();
             SaveMark();
             FilePath = openFileDialog.FileName;
-            Extension = System.IO.Path.GetExtension(FilePath);
-            FileName = System.IO.Path.GetFileName(FilePath);
+            Extension = Path.GetExtension(FilePath);
+            FileName = Path.GetFileName(FilePath);
             ChapterExpander.Header = FileName;
             IsOpen = true;
             AddBookLibrary();
@@ -83,21 +69,20 @@ namespace DeepReadApp
         private void AddBookLibrary()
         {
             var find = false;
-            for (var i = 0; i < _pathBookLibrary.Count; i++)
+            foreach (var unused in _pathBookLibrary.Where(t => FilePath == t))
             {
-                if (FilePath != _pathBookLibrary[i]) continue;
                 find = true;
             }
-            if (find == true) return;
+            if (find) return;
             _pathBookLibrary.Add(FilePath);
-            ImageBrush imageBrush = new ImageBrush(new BitmapImage(
+            var imageBrush = new ImageBrush(new BitmapImage(
                 new Uri(@"redbook.jpg", UriKind.Relative)));
-            var button = new Button()
+            var button = new Button
             {
                 Height = 140, Width = 116.8, Background = imageBrush, FontSize = 7,
-                VerticalContentAlignment = VerticalAlignment.Center
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Content = Path.GetFileName(FilePath)
             };
-            button.Content = System.IO.Path.GetFileName(FilePath);
             button.Click += ClickBookLibrary;
             _wrapPanel.Children.Add(button);
         }
@@ -111,14 +96,14 @@ namespace DeepReadApp
             }
             for (var i = 0; i < maxIndexItem; i++)
             {
-                ImageBrush imageBrush = new ImageBrush(new BitmapImage(
+                var imageBrush = new ImageBrush(new BitmapImage(
                     new Uri(@"redbook.jpg", UriKind.Relative)));
-                Button button = new Button()
+                var button = new Button
                 {
                     Height = 140, Width = 116.8, Background = imageBrush, FontSize = 7,
-                    VerticalContentAlignment = VerticalAlignment.Center
+                    VerticalContentAlignment = VerticalAlignment.Center,
+                    Content = Path.GetFileName(_iniManager.GetPrivateString("Library", Convert.ToString(i)))
                 };
-                button.Content = System.IO.Path.GetFileName(_iniManager.GetPrivateString("Library", Convert.ToString(i)));
                 button.Click += ClickBookLibrary;
                 _wrapPanel.Children.Add(button);
                 _pathBookLibrary.Add(_iniManager.GetPrivateString("Library", Convert.ToString(i)));
@@ -137,19 +122,17 @@ namespace DeepReadApp
         {
             SavePositionInBook_INI();
             SaveMark();
-            bool find = false;
+            var find = false;
             var senderButton = (Button)sender;
-            for (var i = 0; i < _pathBookLibrary.Count; i++)
+            foreach (var t in from t in _pathBookLibrary let temp = Path.GetFileName(t) where temp == Convert.ToString(senderButton.Content) select t)
             {
-                var temp = System.IO.Path.GetFileName(_pathBookLibrary[i]);
-                if (temp != Convert.ToString(senderButton.Content)) continue;
                 find = true;
-                FilePath = _pathBookLibrary[i];
+                FilePath = t;
             }
 
             if (find == false) return;
-            Extension = System.IO.Path.GetExtension(FilePath);
-            FileName = System.IO.Path.GetFileName(FilePath);
+            Extension = Path.GetExtension(FilePath);
+            FileName = Path.GetFileName(FilePath);
             ChapterExpander.Header = FileName;
             IsOpen = true;
             LoadBookFromFile();
@@ -161,9 +144,9 @@ namespace DeepReadApp
         {
             if (!IsOpen) return;
             var zoom = FlowView.Zoom;
-            var curpage = FlowView.MasterPageNumber;
-            _iniManager.WritePrivateString("CurrentPosition", FileName + "/Page", Convert.ToString(curpage));
-            _iniManager.WritePrivateString("CurrentPosition", FileName + "/Zoom", Convert.ToString(zoom));
+            var curPage = FlowView.MasterPageNumber;
+            _iniManager.WritePrivateString("CurrentPosition", FileName + "/Page", Convert.ToString(curPage));
+            _iniManager.WritePrivateString("CurrentPosition", FileName + "/Zoom", Convert.ToString(zoom, CultureInfo.CurrentCulture));
         }
 
         private void RestorePositionInBook_INI()
@@ -192,11 +175,10 @@ namespace DeepReadApp
             {
                 case ".txt" :
                 {
-                    FileStream fileStream;
-                    MemoryStream memoryStream = new MemoryStream();
-                    fileStream = new FileStream(FilePath, FileMode.Open);
+                    var memoryStream = new MemoryStream();
+                    var fileStream = new FileStream(FilePath, FileMode.Open);
                     fileStream.CopyTo(memoryStream);
-                    TextRange range = new TextRange(FlowDoc.ContentStart, FlowDoc.ContentEnd);
+                    var range = new TextRange(FlowDoc.ContentStart, FlowDoc.ContentEnd);
                     range.Load(memoryStream, DataFormats.Text);
                     _panel.Children.Clear();
                     LoadMark();
@@ -206,9 +188,8 @@ namespace DeepReadApp
                 }
                 case ".rtf":
                 {
-                    FileStream fileStream;
-                    fileStream = new FileStream(FilePath, FileMode.Open);
-                    TextRange range = new TextRange(FlowDoc.ContentStart, FlowDoc.ContentEnd);
+                    var fileStream = new FileStream(FilePath, FileMode.Open);
+                    var range = new TextRange(FlowDoc.ContentStart, FlowDoc.ContentEnd);
                     range.Load(fileStream, DataFormats.Rtf);
                     _panel.Children.Clear();
                     LoadMark();
@@ -228,10 +209,10 @@ namespace DeepReadApp
         {
             _listMarkZoom.Add(FlowView.Zoom);
             _listMarkPage.Add(FlowView.MasterPageNumber);
-            Button button = new Button() { Content = Convert.ToString(_listMarkPage.Count + " Bookmark, " + Convert.ToString(FlowView.MasterPageNumber) + " Page"), Tag = _panel.Children.Count};
+            var button = new Button() { Content = Convert.ToString(_listMarkPage.Count + " Bookmark, " + Convert.ToString(FlowView.MasterPageNumber) + " Page"), Tag = _panel.Children.Count};
             
-            ContextMenu contextMenu = new ContextMenu();
-            MenuItem menuItem = new MenuItem();
+            var contextMenu = new ContextMenu();
+            var menuItem = new MenuItem();
             button.ContextMenu = contextMenu;
             menuItem.Header = "Delete";
             menuItem.Tag = Convert.ToString(_panel.Children.Count);
@@ -241,43 +222,6 @@ namespace DeepReadApp
             button.Click += ClickMark;
             _panel.Children.Add(button);
             ChapterExpander.Content = _panel;
-        }
-
-        private void RemoveMarkWindowInitialize()
-        {
-            var label = new Label();
-            var grid = new Grid();
-            var row1 = new RowDefinition();
-            var row2 = new RowDefinition();
-            var row3 = new RowDefinition();
-            _textBoxDelMark.MaxLength = 5;
-            _textBoxDelMark.Background = Brushes.LightSlateGray;
-            label.HorizontalContentAlignment = HorizontalAlignment.Center;
-            label.Content = "Enter bookmark index to delete";
-            label.FontSize = 16;
-            label.VerticalContentAlignment = VerticalAlignment.Center;
-            label.HorizontalContentAlignment = HorizontalAlignment.Center;
-            _buttonDelMark.Height = 40;
-            _buttonDelMark.Width = 110;
-            _buttonDelMark.FontSize = 16;
-            _buttonDelMark.Content = "Delete";
-            _buttonDelMark.Click += RemoveMarkButton;
-            grid.RowDefinitions.Add(row1);
-            grid.RowDefinitions.Add(row2);
-            grid.RowDefinitions.Add(row3);
-            Grid.SetRow(label, 0);
-            Grid.SetRow(_textBoxDelMark, 1);
-            Grid.SetRow(_buttonDelMark, 2);
-            grid.Children.Add(label);
-            grid.Children.Add(_textBoxDelMark);
-            grid.Children.Add(_buttonDelMark);
-            _windowDelMark.Height = 200;
-            _windowDelMark.Width = 300;
-            _windowDelMark.Background = Brushes.SlateGray;
-            _windowDelMark.ResizeMode = ResizeMode.NoResize;
-            _windowDelMark.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            _windowDelMark.Content = grid;
-            _windowDelMark.Closing += RemoveMarkWindowClose;
         }
 
         private void LibraryWindowInitialize()
@@ -290,38 +234,25 @@ namespace DeepReadApp
             _windowLibrary.Closing += LibraryWindowClose;
         }
 
-        private void RemoveMarkWindowClose(object sender, CancelEventArgs e)
-        {
-            e.Cancel = true;
-            _windowDelMark.Hide();
-        }
-
         private void LibraryWindowClose(object sender, CancelEventArgs e)
         {
             e.Cancel = true;
             _windowLibrary.Hide();
         }
 
-        private void RemoveMarkWindowOpen(object sender, RoutedEventArgs e)
-        {
-            if (IsOpen != true) return;
-            _textBoxDelMark.Text = "";
-            if (_windowDelMark.ShowDialog() != true) return;
-        }
-
         private void LibraryWindowOpen(object sender, RoutedEventArgs e)
         {
-            ScrollViewer scrollViewer = new ScrollViewer();
-            ImageBrush imageBrush = new ImageBrush(new BitmapImage(
-                new Uri(@"C:\\Users\\kiril\\OneDrive\\Рабочий стол\\rtedf.jpg", UriKind.Relative)));
-            scrollViewer.Content = _wrapPanel;
+            var scrollViewer = new ScrollViewer
+            {
+                Content = _wrapPanel
+            };
             _windowLibrary.Content = scrollViewer;
-            if (_windowLibrary.ShowDialog() != true) return;
+            _windowLibrary.ShowDialog();
         }
 
         private void RemoveMarkButton(object sender, RoutedEventArgs e)
         {
-            MenuItem it = (MenuItem)sender;
+            var it = (MenuItem)sender;
             _listMarkPage.RemoveAt(Convert.ToInt32(it.Tag));
             _listMarkZoom.RemoveAt(Convert.ToInt32(it.Tag));
             var maxIndexMark = 0;
@@ -338,17 +269,16 @@ namespace DeepReadApp
             _panel.Children.Clear();
             for (var indexMark = 0; indexMark < _listMarkPage.Count; indexMark++)
             {
-                Button button = new Button() { Content = Convert.ToString(indexMark + 1) + " Bookmark, " + Convert.ToString(_listMarkPage[indexMark]) + " Page",
+                var button = new Button() { Content = Convert.ToString(indexMark + 1) + " Bookmark, " + Convert.ToString(_listMarkPage[indexMark]) + " Page",
                     Tag = Convert.ToString(indexMark)};
                 
-                ContextMenu contextMenu = new ContextMenu();
-                MenuItem menuItem = new MenuItem();
+                var contextMenu = new ContextMenu();
+                var menuItem = new MenuItem();
                 button.ContextMenu = contextMenu;
                 menuItem.Header = "Delete";
                 menuItem.Tag = Convert.ToString(indexMark);
                 menuItem.Click += RemoveMarkButton;
                 contextMenu.Items.Add(menuItem);
-                
                 button.Click += ClickMark;
                 _panel.Children.Add(button);
                 ChapterExpander.Content = _panel;
@@ -363,7 +293,7 @@ namespace DeepReadApp
             for (var indexMark = 0; indexMark < _listMarkPage.Count; indexMark++)
             {
                 _iniManager.WritePrivateString("MarkPosition", FileName + "/Zoom/" + Convert.ToString(indexMark),
-                    Convert.ToString(_listMarkZoom[indexMark]));
+                    Convert.ToString(_listMarkZoom[indexMark], CultureInfo.CurrentCulture));
                 _iniManager.WritePrivateString("MarkPosition", FileName + "/Pages/" + Convert.ToString(indexMark),
                     Convert.ToString(_listMarkPage[indexMark]));
             }
@@ -374,7 +304,7 @@ namespace DeepReadApp
             if (!IsOpen) return;
             for (var i = 0; i < _panel.Children.Count; i++)
             {
-                Button button = (Button)sender;
+                var button = (Button)sender;
                 if (Convert.ToInt32(button.Tag) != i) continue;
                 FlowView.Zoom = _listMarkZoom[i];
                 FlowView.GoToPage(_listMarkPage[i]);
@@ -395,11 +325,11 @@ namespace DeepReadApp
             {
                 _listMarkPage.Add(Convert.ToInt32(_iniManager.GetPrivateString("MarkPosition", FileName + "/Pages/" + Convert.ToString(indexMark))));
                 _listMarkZoom.Add(Convert.ToDouble(_iniManager.GetPrivateString("MarkPosition", FileName + "/Zoom/" + Convert.ToString(indexMark))));
-                Button button = new Button() { Content = Convert.ToString(indexMark + 1) + " Bookmark, " + Convert.ToString(_listMarkPage[indexMark]) + " Page",
+                var button = new Button() { Content = Convert.ToString(indexMark + 1) + " Bookmark, " + Convert.ToString(_listMarkPage[indexMark]) + " Page",
                     Tag = Convert.ToString(indexMark)};
                 
-                ContextMenu contextMenu = new ContextMenu();
-                MenuItem menuItem = new MenuItem();
+                var contextMenu = new ContextMenu();
+                var menuItem = new MenuItem();
                 button.ContextMenu = contextMenu;
                 menuItem.Header = "Delete";
                 menuItem.Tag = Convert.ToString(indexMark);
